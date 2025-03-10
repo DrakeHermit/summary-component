36dca928dee82142ba64f3a8ab998bafd49cc603
Jan Kotvaš <jan.kotvas96@gmail.com>
2025-03-10 13:50:18 +0100

    feat: added proper styling for the rendered list items

--- src/components/Category/CategoryList.jsx
+++ src/components/Category/CategoryList.jsx
@@ -5,13 +5,21 @@ export const CategoryList = () => {
   const data = transformJSON();
 
   return (
-    <div>
+    <ul>
       {data.map((item) => (
-        <div key={item.id} className="category__item">
-          <h3>{item.category}</h3>
-          <p>{item.amount}</p>
-        </div>
+        <li key={item.id} className={`${item.color} category__list`}>
+          <div className="category__item">
+            <img src={item.icon} alt={item.category} />
+            <p>{item.category}</p>
+          </div>
+          <div className="category__rating">
+            <p>
+              {item.score} <span className="opacity">/</span>{" "}
+              <span className="opacity">100</span>
+            </p>
+          </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
